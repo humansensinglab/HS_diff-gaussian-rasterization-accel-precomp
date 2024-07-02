c514a35bac664962cf0254fe4510ef12b0a15750
Alejandro <gobberishch@gmail.com>
2024-07-02 00:28:00 +0000

    refactoring

--- setup.py
+++ setup.py
@@ -15,11 +15,11 @@ import os
 os.path.dirname(os.path.abspath(__file__))
 
 setup(
-    name="diff_gaussian_rasterization",
-    packages=['diff_gaussian_rasterization'],
+    name="diff-gaussian-rasterization-accel-precomp",
+    packages=['diff-gaussian-rasterization-accel-precomp'],
     ext_modules=[
         CUDAExtension(
-            name="diff_gaussian_rasterization._C",
+            name="diff-gaussian-rasterization-accel-precomp._C",
             sources=[
             "cuda_rasterizer/rasterizer_impl.cu",
             "cuda_rasterizer/forward.cu",
